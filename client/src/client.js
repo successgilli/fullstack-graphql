fb30254aaa1babe62c0c94c8b2c7b00ca84d9d6c
Awaji-mitop N. Gilbert <successgilli@gmail.com>
2021-03-06 18:22:13 +0100

    completed graphql client

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,24 @@
 import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from "apollo-link"
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
 import { HttpLink } from 'apollo-link-http'
 import gql from 'graphql-tag'
 
 /**
  * Create a new apollo client and export as default
  */
+const http = new HttpLink({uri: 'http://localhost:4000/'})
+const delay = setContext(() => new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), 3000)
+}))
+const link = ApolloLink.from([
+    delay, http
+])
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    cache, link
+})
+
+export default client;
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,21 +1,73 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
-import Loader from '../components/Loader'
+import React, { useState, useEffect } from "react";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import PetsList from "../components/PetsList";
+import NewPetModal from "../components/NewPetModal";
+import Loader from "../components/Loader";
 
+export default function Pets() {
+  const [modal, setModal] = useState(false);
 
-export default function Pets () {
-  const [modal, setModal] = useState(false)
+  const PETS_FIELDS = gql`
+    fragment petsfields on Pet {
+      name
+      id
+      img
+      type
+    }
+  `;
+  const PETS_QUERY = gql`
+    query Pets {
+      pets { ...petsfields }
+    }
+    ${PETS_FIELDS}
+  `;
+  const ADD_PETS_QUERY = gql`
+    mutation AddPet($name: String!, $type: PetType!) {
+      addPet(input: { name: $name, type: $type }) {
+        id
+        type
+        name
+        img
+      }
+    }
+  `;
+  const { data, loading, refetch } = useQuery(PETS_QUERY);
+  const [
+    addPets,
+    { loading: loadingAddedPets, data: newPetsData },
+  ] = useMutation(ADD_PETS_QUERY, {
+    // onCompleted: () => refetch(),
+    update: (cache, { data: { addPet } }) => {
+      const { pets } = cache.readQuery({ query: PETS_QUERY });
+      cache.writeQuery({
+        query: PETS_QUERY,
+        data: { pets: [addPet].concat(pets) },
+      });
+    },
+  });
 
+  const onSubmit = (input) => {
+    setModal(false);
+    addPets({
+      variables: input,
+      optimisticResponse: {
+        __typename: "Mutation",
+        addPet: {
+          __typename: "Pet",
+          id: "hbd978e2nj",
+          name: input.name,
+          type: input.type,
+          img: "",
+        },
+      },
+    });
+  };
 
-  const onSubmit = input => {
-    setModal(false)
-  }
-  
+  if (loading) return <Loader />;
+
   if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
   }
 
   return (
@@ -32,8 +84,8 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
-  )
+  );
 }
